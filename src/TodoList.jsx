3d16044e2b5fa5b909ed668a56dc6bbf36b5f75f
Dario <dario@email.arizona.edu>
2025-07-22 17:48:35 -0700

    week-03

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,15 +1,20 @@
+import TodoListItem from './TodoListItem';
+import { useState } from 'react';
 function TodoList() {
-  const todos = [
+  let todos = [
     { id: 1, title: 'review resources' },
     { id: 2, title: 'take notes' },
     { id: 3, title: 'code out app' },
   ];
+
   return (
-    <ul>
-      {todos.map((todo) => (
-        <li key={todo.id}>{todo.title}</li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {todos.map((todo) => (
+          <TodoListItem key={todo.id} todo={todo} />
+        ))}
+      </ul>
+    </>
   );
 }
 
